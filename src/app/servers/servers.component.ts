bb5d0be9fdb429685c5503f0f348b769407e51fa
WojtekKlosiewicz <wklosiewicz@gmail.com>
2021-08-24 08:14:08 +0200

    2.23 - writing component selector by attribute or class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+@Component({  
   selector: 'app-servers',
+  // write selector by attribute:
+  // selector: '[app-servers]',
+  // write selector by class:
+  // selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
