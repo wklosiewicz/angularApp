02ab6b47914f15fdcbbd01a195ce4553e3ced13e
WojtekKlosiewicz <wklosiewicz@gmail.com>
2021-08-25 17:15:10 +0200

    2.39 - ngStyle directive change element style dynamically

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,7 +8,15 @@ export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'OFFLINE';
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'ONLINE' : 'OFFLINE';
+    }
+
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'ONLINE' ? 'green' : 'red';
     }
 }
